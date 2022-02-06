3af470872f0493bee708b2167a06209cb148113d
hassan.elseoudy <hassan.elseoudy@gmail.com>
2022-02-06 16:35:02 +0200

    :memo: API Documentation.

--- src/main/java/com/bsfdv/transaction/controller/AccountController.java
+++ src/main/java/com/bsfdv/transaction/controller/AccountController.java
@@ -3,6 +3,12 @@ package com.bsfdv.transaction.controller;
 import com.bsfdv.transaction.controller.dto.*;
 import com.bsfdv.transaction.controller.response.AccountResponseDtoV1;
 import com.bsfdv.transaction.service.AccountService;
+import com.bsfdv.transaction.util.error.ApiError;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -30,6 +36,12 @@ public class AccountController {
      * @param accountDto The account to persist
      * @return The persisted account
      */
+    @Operation(summary = "Create New Account")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Created Successfully",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = AccountResponseDtoV1.class))}),
+            @ApiResponse(responseCode = "409", description = "Username or Email Exists",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class))})})
     @PostMapping
     public ResponseEntity<AccountResponseDtoV1> createOne(@Valid @RequestBody SignupDto accountDto) throws Exception {
         AccountResponseDtoV1 responseDtoV1 = AccountResponseDtoV1.toDto(accountService.createOne(accountDto));
@@ -39,11 +51,16 @@ public class AccountController {
     }
 
     /**
-     * Get a specific account's information.
      *
      * @param id The id of the account
      * @return The persisted account.
      */
+    @Operation(summary = "Get a specific account's information")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the Account",
+                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = AccountResponseDtoV1.class)) }),
+            @ApiResponse(responseCode = "404", description = "Account not found",
+                    content = @Content) })
     @GetMapping(path = "/{id}")
     @PreAuthorize("hasRole('USER')")
     public ResponseEntity<AccountResponseDtoV1> getOne(@PathVariable(name = "id") Long id) {
@@ -51,12 +68,15 @@ public class AccountController {
     }
 
     /**
-     * Update a specific account's information.
      *
-     * @param id The id of the account
+     * @param id         The id of the account
      * @param accountDto Account new details
      * @return The persisted account.
      */
+    @Operation(summary = "Update a specific account's information")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Updated Successfully",
+                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = AccountResponseDtoV1.class)) })})
     @PutMapping(path = "/{id}")
     @PreAuthorize("hasRole('USER')")
     public ResponseEntity<AccountResponseDtoV1> updateOne(@Valid @RequestBody UpdateAccountDto accountDto, @PathVariable(name = "id") Long id) {
@@ -69,12 +89,20 @@ public class AccountController {
      * @param id The id of the account
      * @return The persisted account.
      */
+    @Operation(summary = "Delete a specific account's")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Deleted Successfully",
+                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = AccountResponseDtoV1.class)) })})
     @DeleteMapping(path = "/{id}")
     @PreAuthorize("hasRole('USER')")
     public ResponseEntity<AccountResponseDtoV1> deleteOne(@PathVariable(name = "id") Long id) {
         return ResponseEntity.ok(AccountResponseDtoV1.toDto(accountService.deleteOne(id)));
     }
 
+    @Operation(summary = "Login")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Logged in Successfully",
+                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = UserInfoResponse.class)) })})
     @PostMapping("/login")
     public ResponseEntity<UserInfoResponse> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
         UserInfoResponse userInfoResponse = accountService.login(loginRequest);
@@ -82,6 +110,10 @@ public class AccountController {
                 .body(userInfoResponse);
     }
 
+    @Operation(summary = "Logout")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Logged out Successfully",
+                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = MessageResponse.class)) })})
     @PostMapping("/logout")
     @PreAuthorize("hasRole('USER')")
     public ResponseEntity<MessageResponse> logoutUser() {
--- src/main/java/com/bsfdv/transaction/controller/TransactionController.java
+++ src/main/java/com/bsfdv/transaction/controller/TransactionController.java
@@ -3,6 +3,12 @@ package com.bsfdv.transaction.controller;
 import com.bsfdv.transaction.controller.dto.CreateTransactionDto;
 import com.bsfdv.transaction.controller.response.TransactionResponseDtoV1;
 import com.bsfdv.transaction.service.TransactionService;
+import com.bsfdv.transaction.util.error.ApiError;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -28,6 +34,13 @@ public class TransactionController {
      * @param createTransactionDto The transaction to persist
      * @return The persisted transaction
      */
+    @Operation(summary = "Create a new Transaction",
+    description = "Create a new Transaction, either (PAY|DEPOSIT|WITHDRAW) If the transaction is (PAY) you have to provide receiverId.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Transaction created Successfully",
+                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = TransactionResponseDtoV1.class)) }),
+            @ApiResponse(responseCode = "409", description = "Invalid Transaction due to Not Enough Balance.",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ApiError.class))})})
     @PostMapping
     @PreAuthorize("hasRole('USER')")
     public ResponseEntity<TransactionResponseDtoV1> createOne(@Valid @RequestBody CreateTransactionDto createTransactionDto) throws Exception {
@@ -43,6 +56,12 @@ public class TransactionController {
      * @param id The id of the transaction
      * @return The persisted transaction.
      */
+    @Operation(summary = "Get a specific transaction's information")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the Transaction",
+                    content = { @Content(mediaType = "application/json", schema = @Schema(implementation = TransactionResponseDtoV1.class)) }),
+            @ApiResponse(responseCode = "404", description = "Transaction not found",
+                    content = @Content) })
     @GetMapping(path = "/{id}")
     @PreAuthorize("hasRole('USER')")
     public ResponseEntity<TransactionResponseDtoV1> getOne(@PathVariable(name = "id") Long id) {
